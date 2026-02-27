e93103bcb71693085204b9f2a621097c183c8bb9
Marshal Horn <kamocat@gmail.com>
2026-02-26 16:26:54 -0800

    Updated to reflect busio API changes

--- abusio.py
+++ abusio.py
@@ -3,7 +3,7 @@
 import busio
 import asyncio
 
-if not hasattr(busio.I2C, "dma_write"):
+if not hasattr(busio.I2C, "start_write"):
     raise ImportError("This module requires busio with DMA support.")
 
 
@@ -14,15 +14,15 @@ class I2C(busio.I2C):
 
     async def areadfrom_into(self, address, buffer, *, start=0, end=None):
         view = memoryview(buffer)[start:end]
-        d = self.dma_readinto(address, view, end=True)
-        while self.dma_is_busy(d):
+        d = self.start_read(address, view, nostop=nostop)
+        while self.read_isbusy(d):
             await asyncio.sleep(0)
         return len(view)
 
     async def writeto(self, address, buffer, *, start=0, end=None):
         view = memoryview(buffer)[start:end]
-        d = self.dma_write(address, view, end=True)
-        while self.dma_is_busy(d):
+        d = self.start_write(address, view, nostop=nostop)
+        while self.write_isbusy(d):
             await asyncio.sleep(0)
         return len(view)
 
@@ -41,12 +41,12 @@ class I2C(busio.I2C):
         out_view = memoryview(out_buffer)[out_start:out_end]
         in_view = memoryview(in_buffer)[in_start:in_end]
 
-        d = self.dma_write(address, out_view, end=False)
-        while self.dma_is_busy(d):
+        d = self.start_write(address, out_view, nostop=True)
+        while self.write_isbusy(d):
             await asyncio.sleep(0)
 
-        d = self.dma_readinto(address, in_view, end=stop)
-        while self.dma_is_busy(d):
+        d = self.start_read(address, in_view, nostop=not stop)
+        while self.read_isbusy(d):
             await asyncio.sleep(0)
         return len(in_view)
 
@@ -72,21 +72,21 @@ class SPI(busio.SPI):
         if len(out_view) != len(in_view):
             raise ValueError("buffer slices must be same length")
 
-        d = self.dma_write_readinto(out_view, in_view)
-        while self.dma_is_busy(d):
+        d = self.start_transfer(out_view, in_view)
+        while self.transfer_isbusy(d):
             await asyncio.sleep(0)
 
     async def awrite(self, buffer, *, start=0, end=None):
         view = memoryview(buffer)[start:end]
-        d = self.dma_write(view)
-        while self.dma_is_busy(d):
+        d = self.start_transfer(view, None)
+        while self.transfer_isbusy(d):
             await asyncio.sleep(0)
         return len(view)
 
     async def areadinto(self, buffer, *, start=0, end=None, write_value=0):
         view = memoryview(buffer)[start:end]
-        d = self.dma_readinto(view, write_value=write_value)
-        while self.dma_is_busy(d):
+        d = self.start_transfer(None, view)
+        while self.transfer_isbusy(d):
             await asyncio.sleep(0)
         return len(view)
 
@@ -105,7 +105,7 @@ class UART(busio.UART):
         return buf
 
     async def awrite(self, buffer):
-        d = self.dma_write(buffer)
-        while self.dma_is_busy(d):
+        d = self.start_write(buffer)
+        while self.write_isbusy(d):
             await asyncio.sleep(0)
         return len(buffer)
